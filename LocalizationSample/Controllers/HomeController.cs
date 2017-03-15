e81706ea27c88de401f81a66292944fdf7e4f52f
Mattia Dalzocchio <mattia.dalzocchio@gmail.com>
2017-03-15 12:27:38 +0100

    Add project files.

---
+++ LocalizationSample/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Threading;
+
+namespace LocalizationSample.TestNamespace.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string culture)
+        {
+            var ci = new CultureInfo(culture ?? "en-US");
+
+            CultureInfo.CurrentCulture = ci;
+            CultureInfo.CurrentUICulture = ci;
+
+            return View();
+        }
+    }
+}
